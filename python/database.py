54c043f077c4437bcd9f810276f8ccfc94432fc7
bygonexf <dream_star_xf@qq.com>
2024-02-22 00:49:13 +0800

    feat: dockerize

--- python/database.py
+++ python/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///../db/mercari.sqlite3'
+SQLALCHEMY_DATABASE_URL = "sqlite:///" + os.environ.get('SQLITE_PATH')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
